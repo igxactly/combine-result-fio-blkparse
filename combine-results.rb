563efe52bb69530b4c3ea2af856af7663a892e08
Ingu Kang <igxactly@gmail.com>
2016-05-25 22:49:07 +0900

    WIP 1

--- combine-results.rb
+++ combine-results.rb
@@ -9,15 +9,25 @@ Signal.trap("PIPE", "EXIT")
 
 puts "\n\n"
 
+# read fio result
 File.open(ARGV[0], "r") do |f|
     t = f.read()
     o = JSON.parse(t)
 
     puts o
+end
 
-    File.open(ARGV[1], "w") do |outfile|
-       outfile.write(JSON.generate(o))
-    end
+# read yabtar result
+File.open(ARGV[1], "r") do |f|
+    t = f.read()
+    o = JSON.parse(t)
+
+    puts o
+end
+
+# write breakdown result (in which format?)
+File.open(ARGV[2], "w") do |outfile|
+    outfile.write(JSON.generate(o))
 end
 
 puts "\n\n"
