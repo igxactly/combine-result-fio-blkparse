09a6b1771ae553f74cab08356ccd4819db5b3a0b
Ingu Kang <igxactly@gmail.com>
2016-05-26 15:58:19 +0900

    WIP 3 - added run_test.sh

--- combine-results.rb
+++ combine-results.rb
@@ -13,11 +13,24 @@ puts "\n\n"
 File.open(ARGV[0], "r") do |f|
     t = f.read()
     o = JSON.parse(t)
-    #$fio_res = o
-
-    # puts o
 
     $fio_res = {"clat" => Hash[*[["min", "max", "mean"], o["jobs"][0]["read"]["clat"].values_at("min", "max", "mean")].transpose.flatten], "slat" => Hash[*[["min", "max", "mean"], o["jobs"][0]["read"]["slat"].values_at("min", "max", "mean")].transpose.flatten]}
+
+# jobs [ {read, write, ...} ]
+#
+# grand_total_sum_slat = 0
+# grand_total_sum_clat = 0
+# grand_total_ios = 0
+#
+# for job in jobs
+#         grand_total_sum_slat += job.read.total_ios * job.read.slat.mean
+#             grand_total_sum_clat += job.read.total_ios * job.read.clat.mean
+#                 grand_total_ios += job.read.total_ios
+# end
+#
+# grand_avg_slat = grand_total_sum_slat.to_f / grand_total_ios
+# grand_avg_clat = grand_total_sum_clat.to_f / grand_total_ios
+
 end
 
 # read yabtar result
@@ -25,8 +38,6 @@ File.open(ARGV[1], "r") do |f|
     t = f.read()
     o = JSON.parse(t)
     $yabtar_res = o
-
-    # puts o
 end
 
 $combined_res = {"fio_res"=>$fio_res, "yabtar_res"=>$yabtar_res}
@@ -40,5 +51,4 @@ File.open(ARGV[2], "w") do |f|
 end
 
 puts "\n\n"
-
 
---
+++ run_test.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# for all_test_cases
+# do
+#     run blktrace
+#         run fio_script > result_fio.json
+#     stop blktrace
+#
+#     blkparse -i nvme0n1 -d all.blktrace
+#     yabtar all.blktrace > result_blktrace.json
+#
+#     result_fio.json + result_blktrace.json --> result_latency_breakdown.json
+# done
+
+# echo "$(date -Ins);${kernel};${n_bs};${n_qd};$(${single_test_script} ${testname} ${n_bs}k ${n_qd} ${n_runt} ${format} ${n_jobs} | sed 's/[0-9]\+\.[0-9]\{6\}%=\([0-9]\+;\)/\1/g')" | tee result_${date}_${testname}_${i}.csv;
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+single_test_script=~/fio_presets/do_test_read.sh
+
+dev=/dev/nvme0n1
+n_runt=1;
+n_jobs=1; #8;
+format="json";
+kernel=$(uname -r | sed 's/\([0-9]\.[0-9]\+\).\+/\1/g');
+date=$(date -I)
+
+for i in $(seq 1);
+do
+
+for n_bs in 4 128; # for each block size
+do
+    for n_qd in 1 1024; # for each queue depth
+    do
+        testname="${kernel}_job${n_jobs}_bs${n_bs}k_qd${n_qd}";
+        mkdir -p ${testname}; cd ${testname};
+
+        ### start trace
+        echo "starting blktrace/fio..."
+        sleep 3; blktrace ${dev} & PID_BLKTRACE=$!;
+
+        ${single_test_script} ${testname} ${n_bs}k ${n_qd} ${n_runt} ${format} ${n_jobs} > fio_result.json; sync;
+
+        sleep 3; kill ${PID_BLKTRACE}; fg; sync;
+        echo "blktrace/fio done";
+        ### end trace
+
+        # parse and analyze block trace
+        echo "parsing results...";
+        blkparse -O -i nvme0n1 -d all.blktrace; sync;
+        yabtar.rb all.blktrace yabtar_result.json | tail; sync;
+
+        # final result
+        combine-results.rb fio_result.json yabtar_result.json breakdown.json > breakdown.txt; sync;
+        echo "parsing and anylysis done"
+        cd ..;
+    done;
+done;
+done;
