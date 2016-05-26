2f296a4f66e06202819940cba9d87d885b4582b6
Ingu Kang <igxactly@gmail.com>
2016-05-26 12:54:01 +0900

    WIP 2

--- combine-results.rb
+++ combine-results.rb
@@ -5,7 +5,7 @@ require 'json'
 #################
 # main program flow starts here
 
-Signal.trap("PIPE", "EXIT")
+# Signal.trap("PIPE", "EXIT")
 
 puts "\n\n"
 
@@ -13,21 +13,30 @@ puts "\n\n"
 File.open(ARGV[0], "r") do |f|
     t = f.read()
     o = JSON.parse(t)
+    #$fio_res = o
 
-    puts o
+    # puts o
+
+    $fio_res = {"clat" => Hash[*[["min", "max", "mean"], o["jobs"][0]["read"]["clat"].values_at("min", "max", "mean")].transpose.flatten], "slat" => Hash[*[["min", "max", "mean"], o["jobs"][0]["read"]["slat"].values_at("min", "max", "mean")].transpose.flatten]}
 end
 
 # read yabtar result
 File.open(ARGV[1], "r") do |f|
     t = f.read()
     o = JSON.parse(t)
+    $yabtar_res = o
 
-    puts o
+    # puts o
 end
 
+$combined_res = {"fio_res"=>$fio_res, "yabtar_res"=>$yabtar_res}
+
 # write breakdown result (in which format?)
-File.open(ARGV[2], "w") do |outfile|
-    outfile.write(JSON.generate(o))
+File.open(ARGV[2], "w") do |f|
+    t = JSON.generate($combined_res)
+    f.write(t)
+
+    puts t
 end
 
 puts "\n\n"
